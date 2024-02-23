a2040058c6135270d3a5dfe87d4c8650c2c678de
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-02-23 16:06:08 +0200

    Second class

---
+++ ConsoleApp2/ConsoleApp2/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class BankAccount
+    {
+        private string name;
+        private int sold;
+
+        public void CreateAccount()
+        {
+            Console.Write("Owner: "); name = Console.ReadLine();
+            sold = 0;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{name} has {sold}RON");
+        }
+
+        public void Deposit(int amount)
+        {
+            sold += amount;
+        }
+
+        public bool Withdrawal(int amount)
+        {
+            if (sold > amount)
+            {
+                sold -= amount;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool TransferTo(BankAccount destination, int amount)
+        {
+            if (Withdrawal(amount))
+            {
+                destination.Deposit(amount);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp2;
+
+BankAccount bankAccount = new BankAccount();
+BankAccount bc2 = new BankAccount();
+bankAccount.CreateAccount();
+bc2.CreateAccount();
+bankAccount.Print();
+bankAccount.Deposit(100);
+bankAccount.Print();
+
+while (true)
+{
+    if (bankAccount.Withdrawal(int.Parse(Console.ReadLine()))){
+        bankAccount.Print();
+        break;
+    } else
+    {
+        Console.WriteLine("Nu aveti banii in contul");
+    }
+}
+
+while (true)
+{
+    if(bankAccount.TransferTo(bc2, int.Parse(Console.ReadLine())))
+    {
+        bankAccount.Print();
+        bc2.Print();
+        break;
+    } else
+    {
+        Console.WriteLine("Nu aveti banii in contul");
+    }
+}
