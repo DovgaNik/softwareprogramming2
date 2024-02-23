16eb7f9b63eb992cff66c381ce8c353e0b03a4f8
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-02-23 15:04:29 +0200

    First class

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+
+Student student1 = new Student();
+Student student2 = new Student();
+
+student1.set();
+student2.set();
+
+student1.get();
+student2.get();
+
+System.Console.WriteLine(student1.passed());
+System.Console.WriteLine(student2.passed());
+
+student1.changeGroup(int.Parse(System.Console.ReadLine()));
+student2.changeGroup(int.Parse(System.Console.ReadLine()));
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        string name;
+        float grade;
+        int group;
+        
+        public void get()
+        {
+            System.Console.WriteLine($"Student {name} is in group {group} and has a grade of {grade}");
+        }
+
+        public void set()
+        {
+            System.Console.Write("Enter name: ");
+            name = System.Console.ReadLine();
+            System.Console.Write("Enter group: ");
+            group = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Enter grade: ");
+            grade = float.Parse(System.Console.ReadLine());
+        }
+
+        public bool passed()
+        {
+            return grade >= 5;
+        }
+
+        public void changeGroup(int newGroup)
+        {
+            group = newGroup;
+        }
+
+        public void changeGrade(float bonus)
+        {
+            grade += bonus;
+            if (grade > 10)
+                grade = 10;
+        }
+    }
+}
