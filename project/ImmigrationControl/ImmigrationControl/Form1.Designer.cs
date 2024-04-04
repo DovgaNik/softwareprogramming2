b8406e7b67592ed941cadebfb2d0ffa01dee6961
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-04-04 20:06:09 +0300

    Added basic useradd functionality to the semestrial project

--- project/ImmigrationControl/ImmigrationControl/Form1.Designer.cs
+++ project/ImmigrationControl/ImmigrationControl/Form1.Designer.cs
@@ -29,33 +29,92 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(268, 114);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(84, 20);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Immigrants";
             // 
-            // label2
+            // listBox1
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(268, 195);
-            label2.Name = "label2";
-            label2.Size = new Size(50, 20);
-            label2.TabIndex = 1;
-            label2.Text = "label2";
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 32);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(230, 404);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(248, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(248, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(248, 100);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(125, 27);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(248, 133);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(248, 167);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(248, 201);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -66,6 +125,12 @@
         #endregion
 
         private Label label1;
-        private Label label2;
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox2;
     }
 }
--- project/ImmigrationControl/ImmigrationControl/Form1.cs
+++ project/ImmigrationControl/ImmigrationControl/Form1.cs
@@ -1,20 +1,44 @@
+using immigrationLib;
 namespace ImmigrationControl
 {
     public partial class Form1 : Form
     {
+        ImmigrantList immigrants;
         public Form1()
         {
-            Login loginForm = new Login();
+            //Login loginForm = new Login();
 
-            if (loginForm.ShowDialog() == DialogResult.Cancel)
-            {
-                MessageBox.Show("To use the application you must be logged in!!!");
-                System.Environment.Exit(1);
-            }
-            else
-                InitializeComponent();
-        
+            //if (loginForm.ShowDialog() == DialogResult.Cancel)
+            //{
+            //    MessageBox.Show("To use the application you must be logged in!!!");
+            //    System.Environment.Exit(1);
+            //}
+            //else
+            InitializeComponent();
+            immigrants = new ImmigrantList("immigrants.json");
+            comboBox1.DataSource = new BindingSource(Countries.countries, null);
+            comboBox1.DisplayMember = "Value";
+            comboBox1.ValueMember = "Key";
+            comboBox2.DataSource = new BindingSource(Countries.countries, null);
+            comboBox2.DisplayMember = "Value";
+            comboBox2.ValueMember = "Key";
+
+            reloadListBox(listBox1, immigrants);
+
         }
 
+        private void reloadListBox(ListBox listbox, ImmigrantList list)
+        {
+            listbox.Items.Clear();
+            foreach (Immigrant immigrant in list)
+                listbox.Items.Add(immigrant);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Immigrant newImmigrant = new Immigrant(textBox1.Text, dateTimePicker1.Value.Date, comboBox1.TabIndex, comboBox2.TabIndex, textBox2.Text);
+            immigrants.Add(newImmigrant);
+            listBox1.Items.Add(newImmigrant.Name);
+        }
     }
 }
